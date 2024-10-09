4935ad92bb32e5ae4aeb4de169be74a534e44c48
cpe-liam <cpe.williamwallacedizon@gmail.com>
2024-10-09 01:58:31 -0400

    bizkit interview

--- phasebook/search.py
+++ phasebook/search.py
@@ -1,30 +1,58 @@
-from flask import Blueprint, request
+from flask import Blueprint, request,jsonify
 
 from .data.search_data import USERS
 
-
 bp = Blueprint("search", __name__, url_prefix="/search")
-
 
 @bp.route("")
 def search():
-    return search_users(request.args.to_dict()), 200
-
+    return jsonify(search_users(request.args.to_dict())), 200
 
 def search_users(args):
-    """Search users database
+    user_id = args.get('id')
+    name = args.get('name', '').lower()  
+    age = args.get('age')
+    occupation = args.get('occupation', '').lower()  
+    
+    if age:
+        age = int(age)
 
-    Parameters:
-        args: a dictionary containing the following search parameters:
-            id: string
-            name: string
-            age: string
-            occupation: string
+    id_matches = []
+    name_matches = []
+    age_matches = []
+    occupation_matches = []
+    
+    if user_id:
+        for user in USERS:
+            if user['id'] == user_id:
+                id_matches.append(format_user(user))  
+                break  
 
-    Returns:
-        a list of users that match the search parameters
-    """
+    for user in USERS:
+        name_match = name in user['name'].lower() if name else False
+        age_match = (user['age'] in range(age - 1, age + 2)) if age else False
+        occupation_match = occupation in user['occupation'].lower() if occupation else False
+        if name_match:
+            name_matches.append(format_user(user))
+        if age_match:
+            age_matches.append(format_user(user))
+        if occupation_match:
+            occupation_matches.append(format_user(user))
 
-    # Implement search here!
+    results = id_matches + name_matches + age_matches + occupation_matches
+    unique_results = []
+    seen_ids = set()
+    for user in results:
+        if user["id"] not in seen_ids:
+            seen_ids.add(user["id"])
+            unique_results.append(user)
 
-    return USERS
+    return unique_results
+
+def format_user(user):
+    return {
+        "id": user["id"],
+        "name": user["name"],
+        "age": user["age"],
+        "occupation": user["occupation"]
+    }
